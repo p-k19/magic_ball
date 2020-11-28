82bbd3c33db9079b4c7ddea34dcb1bb24aab68e9
prince <47668900+p-k19@users.noreply.github.com>
2020-11-28 14:56:57 +0530

    Update magic.js

--- magic.js
+++ magic.js
@@ -1,18 +1,24 @@
 
+
+
+
+var name=document.getElementById('name').value;
+
 function myFunction()    //For the userName input here!!
 {    debugger;
-    var name=document.getElementById('name').value;
+    
     var uname=(name == " ") ? "Hello Dear!":document.getElementById('getName').innerHTML="Hello" + " " +name  + "!";
 }
 
-function myFunction2(){       //For the Questioning and  validating the question
+function myFunction2(){   
+    debugger;    //For the Questioning and  validating the question
     var question=document.getElementById('question').value;
-    if(question == undefined)
+    if(question == "")
         {
-              document.getElementById("getanser").innerHTML="Enter the question first";
+              document.getElementById("getanser").innerHTML="Enter the question First!";
         }
-               else if(question.length >10 && isNaN(question))
-              {
+               else if(question.length >10 && isNaN(question) && question != "Who will be the prefect groom for me")
+              {      var name=document.getElementById('name').value;
                     var  anser=["It is certain","It is decidedly so." ,"Without a doubt","Yes – definitely.","Most likely.","Cannot predict now.","Better not tell you now.","Better not tell you now","Outlook good.",
                       "Reply hazy, try again.",
                     "Signs point to yes.",
@@ -23,7 +29,14 @@ function myFunction2(){       //For the Questioning and  validating the question
                     ];
                     var randomNo=Math.floor(Math.random() * 12);
                     var eightBall=anser[randomNo];
-                    document.getElementById("getanser").innerHTML=eightBall;
+                    document.getElementById("getanser").innerHTML= name+ "," + " " +eightBall;
+                }
+
+                else if(question == "Who will be the prefect groom for me")       ////Funny part ;-)
+                {
+                  var eightBall="Prince is the perfect groom for you!";
+                  document.getElementById("getanser").innerHTML=eightBall;
+                    
                 }
   else   {
          document.getElementById("getanser").innerHTML="Enter the valid question";
@@ -37,3 +50,4 @@ function reset(){                                        //For resting the  form
     document.getElementsById("name").innerHTML=" ";
 }
 
+ 
