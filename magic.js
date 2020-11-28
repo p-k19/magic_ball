73dff4268fe02be4154f9359e5190cb47f0e6373
prince <47668900+p-k19@users.noreply.github.com>
2020-11-28 11:58:43 +0530

    Update magic.js

--- magic.js
+++ magic.js
@@ -1,18 +1,39 @@
 
-function myFunction()
+function myFunction()    //For the userName input here!!
 {    debugger;
     var name=document.getElementById('name').value;
     var uname=(name == " ") ? "Hello Dear!":document.getElementById('getName').innerHTML="Hello" + " " +name  + "!";
 }
 
-function myFunction2(){
-var  anser=["It is certain","It is decidedly so." ,"Without a doubt","Yes – definitely.","Most likely.","Cannot predict now.","Better not tell you now.","Better not tell you now" ]
-var randomNo=Math.floor(Math.random() * 5);
-var eightBall=anser[randomNo];
-document.getElementById("getanser").innerHTML=eightBall;
+function myFunction2(){       //For the Questioning and  validating the question
+    var question=document.getElementById('question').value;
+    if(question == undefined)
+        {
+              document.getElementById("getanser").innerHTML="Enter the question first";
+        }
+               else if(question.length >10 && isNaN(question))
+              {
+                    var  anser=["It is certain","It is decidedly so." ,"Without a doubt","Yes – definitely.","Most likely.","Cannot predict now.","Better not tell you now.","Better not tell you now","Outlook good.",
+                      "Reply hazy, try again.",
+                    "Signs point to yes.",
+                    "Very doubtful.",
+                    "Without a doubt.",
+                    "Yes.",
+                     "You may rely on it."
+                    ];
+                    var randomNo=Math.floor(Math.random() * 12);
+                    var eightBall=anser[randomNo];
+                    document.getElementById("getanser").innerHTML=eightBall;
+                }
+  else   {
+         document.getElementById("getanser").innerHTML="Enter the valid question";
+       }
+
 }
-function reset(){
+
+function reset(){                                        //For resting the  forms
     document.getElementById("getanser").innerHTML=" ";
     document.getElementById("getName").innerHTML=" ";
     document.getElementsById("name").innerHTML=" ";
 }
+
